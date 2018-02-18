b5030afe5391a35e2b02ee35187db0d2588a6e56
Mert Ahmet Yiğit <mayigit@icloud.com>
2018-02-18 15:07:53 +0300

    Initial Commit

---
+++ Mersgram/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mersgram
+//
+//  Created by Mert Ahmet Yiğit on 18.02.2018.
+//  Copyright © 2018 Mert Yiğit. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mersgram/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mersgram
+//
+//  Created by Mert Ahmet Yiğit on 18.02.2018.
+//  Copyright © 2018 Mert Yiğit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

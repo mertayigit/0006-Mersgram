1481ef6679533f2725220009fade13bea7ee6bc6
Mert Ahmet Yiğit <mayigit@icloud.com>
2018-02-19 18:57:13 +0300

    Lets Go

---
+++ Mersgram/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  Mersgram
+//
+//  Created by Mert Ahmet Yiğit on 18.02.2018.
+//  Copyright © 2018 Mert Yiğit. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
